1f81d8ef624af80057864c2fe1e7c6c93c7ac727
Carlos <carlos@prolydian.com>
2023-07-08 15:58:31 -0400

    fix footer responsiveness

--- src/routes/components/Footer.svelte
+++ src/routes/components/Footer.svelte
@@ -5,9 +5,9 @@
 <footer class="w-full py-12 md:py-24 bg-teal-600 text-white overflow-hidden z-0">
 	<div class="container mx-auto px-4 relative flex items-center space-y-12 flex-col text-center">
 		<div
-			class="flex flex-column space-y-2 md:space-y-0 md:space-x-8 md:flex-row md:items-center"
+			class="flex flex-col-reverse md:space-y-0 md:space-x-8 md:flex-row md:items-center"
 		>
-			<p class="font-display">
+			<p class="font-display mt-8 md:mt-0 opacity-80">
 				© {new Date().getFullYear()} Glimmr LLC / All rights reserved.
 			</p>
 			<DownloadButton size={4} background="light" showFileSize={true} />
