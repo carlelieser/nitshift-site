e50294804265737263d9e39b63fc0460c1bf5834
Carlos <carloseliesersantos@gmail.com>
2025-01-01 03:11:44 -0500

    fix bug report sending

--- src/routes/api/bugs/+server.ts
+++ src/routes/api/bugs/+server.ts
@@ -1,11 +1,8 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
-import * as nodemailer from "nodemailer";
-import { config, getBugReportConfig } from "$lib/server/mailer";
+import { sendBugReport } from "$lib/server/mailer";
 
 export const POST: RequestHandler = async ({ request }) => {
 	const { title, description, machine } = await request.json();
-	const transporter = nodemailer.createTransport(config);
-	const options = await getBugReportConfig(title, description, machine);
-	await transporter.sendMail(options);
+	await sendBugReport(title, description, machine);
 	return json({ success: true });
 };
