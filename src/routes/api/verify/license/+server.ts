90ce0cc213f29a877a0f718888dc6dc96899a24f
Carlos <carloseliesersantos@gmail.com>
2025-01-01 05:35:14 -0500

    send license verified email after license verification, not after email verification

--- src/routes/api/verify/license/+server.ts
+++ src/routes/api/verify/license/+server.ts
@@ -1,8 +1,10 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
 import { findCustomerByEmail, getLatestPaymentIntent } from "$lib/server/stripe";
 import { handleRequest } from "$lib/server/utils";
+import { sendLicenseVerifiedEmail } from "$lib/server/mailer";
+import admin from "firebase-admin";
 
-export const GET: RequestHandler = handleRequest(async ({ url }) => {
+export const GET: RequestHandler = handleRequest(async ({ url, locals, fetch }) => {
 	const { searchParams } = url;
 	const email = searchParams.get("email");
 
@@ -10,6 +12,18 @@ export const GET: RequestHandler = handleRequest(async ({ url }) => {
 		const customer = await findCustomerByEmail(email);
 		const paymentIntent = await getLatestPaymentIntent(customer.id);
 		const success = paymentIntent.status === "succeeded";
+
+		if (success) {
+			await sendLicenseVerifiedEmail(email);
+			await admin
+				.firestore()
+				.collection("users")
+				.doc(locals.user?.id as string)
+				.set(
+					{ license: "premium", trialAvailability: false, trialStartDate: null },
+					{ merge: true }
+				);
+		}
 
 		return json(
 			{
