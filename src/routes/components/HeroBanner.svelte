3fd97df0456532c4c182f236fd808c25873094c0
Carlos <carloseliesersantos@gmail.com>
2025-01-03 06:08:17 -0500

    fix landing page on mobile

--- src/routes/components/HeroBanner.svelte
+++ src/routes/components/HeroBanner.svelte
@@ -40,7 +40,7 @@
 
 <svelte:window bind:scrollY />
 
-<div class="section-container bg-white text-white relative z-0 !pt-0 !-mt-32">
+<div class="section-container bg-white text-white relative z-0 !pt-0 !-mt-24 md:!-mt-32">
 	<div
 		class="section p-12 md:p-24 !py-36 !pt-44 bg-zinc-900 rounded-3xl relative z-20 bg-repeat bg-contain"
 	>
