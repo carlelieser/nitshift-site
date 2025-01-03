7e6eaabaa5430d38369916e081c7755eb70d1fa5
Carlos <carloseliesersantos@gmail.com>
2025-01-03 02:53:18 -0500

    fix asset fetching

--- src/routes/images/[filename]/+server.ts
+++ src/routes/images/[filename]/+server.ts
@@ -1,20 +1,33 @@
 import { error, type RequestHandler } from "@sveltejs/kit";
 import * as path from "node:path";
-import { read } from "$app/server";
+import { isDevelopment } from "$lib/server/utils";
+import * as fs from "node:fs";
+import { Readable } from "node:stream";
+import * as os from "node:os";
 
 const modules = import.meta.glob("$lib/client/assets/*.{ico,png,svg}");
 
 export const GET: RequestHandler = async ({ params }) => {
 	const filename = params.filename as string;
-	const module = Object.keys(modules).find(
+	const modulePath = Object.keys(modules).find(
 		(modulePath) => path.basename(modulePath) === filename
 	);
 
-	if (!module) {
-		error(404, {
+	if (!modulePath) {
+		return error(404, {
 			message: "File not found"
 		});
 	}
 
-	return read(module);
+	const module = (await modules[modulePath]()) as { default: string };
+	const basePath = isDevelopment
+		? path.join(import.meta.url, "..", "..", "..", "..", "..")
+		: path.join(import.meta.url, "..", "..", "..", "client");
+	const fileUrl = new URL(path.join(basePath, module.default));
+	const filePath = path.normalize(fileUrl.pathname);
+	const sanitizedFilePath = os.platform() === "win32" ? filePath.replace("\\", "") : filePath;
+	const stream = fs.createReadStream(sanitizedFilePath);
+	const webStream = Readable.toWeb(stream) as BodyInit;
+
+	return new Response(webStream);
 };
