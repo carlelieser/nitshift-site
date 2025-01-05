af69229052274546f5c11b5190f3316ef9385873
Carlos <carloseliesersantos@gmail.com>
2025-01-05 08:41:20 -0500

    implement stripe signature verification

--- src/routes/stripe/webhook/+server.ts
+++ src/routes/stripe/webhook/+server.ts
@@ -1,12 +1,28 @@
-import { json, type RequestHandler } from "@sveltejs/kit";
+import { error, json, type RequestHandler } from "@sveltejs/kit";
 import { generateLicenseAndNotifyUser } from "$lib/server/license";
+import { stripe } from "$lib/server/stripe";
+import { STRIPE_WEBHOOK_SECRET } from "$env/static/private";
 
 export const POST: RequestHandler = async ({ request }) => {
-	const body = await request.json();
-	const type = body.type;
+	const sig = request.headers.get("stripe-signature") as string;
+
+	let event;
+
+	try {
+		event = stripe.webhooks.constructEvent(
+			Buffer.from(await request.arrayBuffer()),
+			sig,
+			STRIPE_WEBHOOK_SECRET
+		);
+	} catch (err: any) {
+		console.error(err);
+		return error(400, { message: `Webhook Error: ${err.message}` });
+	}
+
+	const type = event.type;
 
 	if (type === "payment_intent.succeeded") {
-		await generateLicenseAndNotifyUser(body.data.object);
+		await generateLicenseAndNotifyUser(event.data.object);
 	}
 
 	return json({
