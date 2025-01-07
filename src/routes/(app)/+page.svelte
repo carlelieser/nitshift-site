e94aad810273837152b0d7a9f1a1884c09517747
Carlos <carloseliesersantos@gmail.com>
2025-01-07 08:24:04 -0500

    remove unused style tag

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -19,19 +19,3 @@
 <Pricing />
 <Separator />
 <FAQ />
-
-<style>
-    @keyframes fade-in {
-        0% {
-            opacity: 0;
-        }
-        100% {
-            opacity: 100%;
-        }
-    }
-
-    .fade-in {
-        opacity: 0;
-        animation: fade-in 0.5s ease-in-out forwards;
-    }
-</style>
