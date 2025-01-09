3cebea82f58509809c2e354b0df563f419d8bbb3
Carlos <carloseliesersantos@gmail.com>
2025-01-09 07:07:38 -0500

    run ensureUserId in parallel for faster processing

--- src/routes/api/admin/functions/ensure-user-id/+server.ts
+++ src/routes/api/admin/functions/ensure-user-id/+server.ts
@@ -1,26 +1,31 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
 import { handleRequest } from "$lib/server/utils";
-import admin from "firebase-admin";
+import admin, { firestore } from "firebase-admin";
+import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
+
+const ensureUserId = async (userDoc: QueryDocumentSnapshot) => {
+	const user = userDoc.data();
+	if (userDoc.id.includes("@glimmr.com")) {
+		const userCollection = admin.firestore().collection("users");
+		await userDoc.ref.delete();
+		await userCollection.doc(user.id).set(user);
+	}
+};
 
 /*
 	Ensures all user docs have a standardized id.
 	Since migrating firebase auth/user logic to the server, users no longer use @glimmr.com ids.
  */
-const ensureUserId = async () => {
+const ensureAllUserIds = async () => {
 	const userCollection = admin.firestore().collection("users");
 	const users = await userCollection.get();
+	const promises = users.docs.map(ensureUserId);
 
-	for (const userDoc of users.docs) {
-		const user = userDoc.data();
-		if (userDoc.id.includes("@glimmr.com")) {
-			await userDoc.ref.delete();
-			await userCollection.doc(user.id).set(user);
-		}
-	}
+	await Promise.all(promises);
 };
 
 export const GET: RequestHandler = handleRequest(async () => {
-	await ensureUserId();
+	await ensureAllUserIds();
 	return json({
 		success: true
 	});
