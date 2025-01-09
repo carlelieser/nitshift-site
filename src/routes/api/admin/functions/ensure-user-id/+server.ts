13b144192f5ceda47b9f4a0eb113d1d6737e042e
Carlos <carloseliesersantos@gmail.com>
2025-01-09 09:09:43 -0500

    move function logic into request handler

--- src/routes/api/admin/functions/ensure-user-id/+server.ts
+++ src/routes/api/admin/functions/ensure-user-id/+server.ts
@@ -16,16 +16,13 @@ const ensureUserId = async (userDoc: QueryDocumentSnapshot) => {
 	Ensures all user docs have a standardized id.
 	Since migrating firebase auth/user logic to the server, users no longer use @glimmr.com ids.
  */
-const ensureAllUserIds = async () => {
+export const GET: RequestHandler = handleRequest(async () => {
 	const userCollection = admin.firestore().collection("users");
 	const users = await userCollection.get();
 	const promises = users.docs.map(ensureUserId);
 
 	await Promise.all(promises);
-};
 
-export const GET: RequestHandler = handleRequest(async () => {
-	await ensureAllUserIds();
 	return json({
 		success: true
 	});
