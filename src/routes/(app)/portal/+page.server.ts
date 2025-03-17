0e5a7a2fcba17444c1d0ef2fdbf9429ca33f0f97
Carlos <carloseliesersantos@gmail.com>
2025-03-17 17:59:48 -0400

    handle firebase timestamp objects correctly

--- src/routes/(app)/portal/+page.server.ts
+++ src/routes/(app)/portal/+page.server.ts
@@ -1,5 +1,7 @@
 import type { PageServerLoad } from "./$types";
 import { get, getUsersByEmail, InstallerCollection, LicenseCollection } from "$lib/server/firebase";
+import admin from "firebase-admin";
+import Timestamp = admin.firestore.Timestamp;
 
 export const load: PageServerLoad = async ({ locals, depends }) => {
 	if (locals.user) {
@@ -8,13 +10,17 @@ export const load: PageServerLoad = async ({ locals, depends }) => {
 		const installer = await get(InstallerCollection(), locals.user.email);
 
 		if (license.data) {
-			const licenseIssuedOn = license.data.issuedOn || license.doc.createTime?.toDate();
+			let licenseIssuedOn = license.data.issuedOn || license.doc.createTime;
 			const devices = deviceQuery.docs.map((doc) => doc.data()?.id);
 
 			let installerInfo = installer.data;
 
 			if (installer.data?.cancelled) {
 				installerInfo = undefined;
+			}
+
+			if (licenseIssuedOn instanceof Timestamp) {
+				licenseIssuedOn = licenseIssuedOn.toDate();
 			}
 
 			return {
