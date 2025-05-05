2eb25ee687249619e7ebe9d98f8ff034767c8c89
Carlos Santos <carloseliesersantos@gmail.com>
2025-05-05 15:25:13 -0400

    add progress logs

--- upload.js
+++ upload.js
@@ -11,20 +11,33 @@ ssh.connect({
 	password: process.env.SERVER_PASSWORD
 })
 	.then(() => {
-		return ssh.putDirectory("build", "/var/www/html");
+		console.log("Uploading build...");
+		return ssh.putDirectory("build", "/var/www/html", {
+			concurrency: 1,
+			tick: (localFile, remoteFile, error) => {
+				console.log(`Uploading file: ${localFile} -> ${remoteFile}`);
+				if (error) {
+					console.error(error);
+				}
+			}
+		});
 	})
 	.then(() => {
+		console.log("Uploading package.json...");
 		return ssh.putFile("package.json", "/var/www/html/package.json");
 	})
 	.then(() => {
+		console.log("Installing NPM modules...");
 		return ssh.execCommand("npm install", {
 			cwd: "/var/www/html"
 		});
 	})
 	.then(() => {
+		console.log("Correcting directory access...");
 		return ssh.execCommand("chmod -R 755 /var/www/html");
 	})
 	.then(() => {
+		console.log("Restarting server...");
 		return ssh.execCommand("service apache2 restart");
 	})
 	.then(() => {
